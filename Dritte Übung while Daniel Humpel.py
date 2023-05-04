fb377219d5e00e5ac48bc313c75e30b28cb73c27
OHYeeah <132549049+OHYeeah@users.noreply.github.com>
2023-05-04 12:13:19 +0200

    Add files via upload

---
+++ Dritte Übung while Daniel Humpel.py
@@ -0,0 +1,23 @@
+a = int(input ("Zahl 1: ")) #int a
+b = int(input ("Zahl 2: ")) #int b
+
+print ("Sum a+b = ", a+b)
+print ("Dif a-b = ", a-b)
+print ("Dif b-a = ", b-a)
+print ("Dif b-a =", b-a)
+print ("Pro a*b = " , a*b)
+
+if a == 0 and b != 0:
+    print ("Quo a/b = ",a/b)
+    
+if a != 0 and b == 0:
+    print ("Quo b/a = ", b/a)
+    
+while  b == 0 or a == 0:
+    print ("DIV 0!")
+    b = int(input ("neue Zahl 2 eingeben:"))
+    a = int(input ("neue Zahl 1 eingeben:"))
+    
+
+print ("Quo a/b = ",a/b)
+print ("Quo b/a = ",a/b)
