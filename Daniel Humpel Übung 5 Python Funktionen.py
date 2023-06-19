a26a8d3d88e113e176ac945162d4ca524f36d9db
OHYeeah <132549049+OHYeeah@users.noreply.github.com>
2023-06-19 16:57:19 +0200

    Add files via upload

---
+++ Daniel Humpel Übung 5 Python Funktionen.py
@@ -0,0 +1,8 @@
+def begruessung(namen):
+    for name in namen:
+        print("Hallo", name)
+        print("Schön dich zu sehen!")
+        print("Viel Spaß mit dem Programm")
+
+namen = ["anton", "berta", "cesar", "dora"]
+begruessung(namen)
---
+++ Übung 6 Daniel Humpel.py
@@ -0,0 +1,16 @@
+def summe(a, b):
+    c = a + b
+    return c
+
+def differenz(a, b):
+    c = a - b
+    return c
+
+a = 2
+b = 1
+
+ergebnis_summe = summe(a, b)
+ergebnis_differenz = differenz(a, b)
+
+print("Summe ", a, "und", b, "ist:", ergebnis_summe)
+print("Differenz ", a, "und", b, "ist:", ergebnis_differenz)
