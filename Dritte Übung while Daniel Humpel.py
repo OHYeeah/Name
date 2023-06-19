88060b04d26112dc514dfbfb68492bd4443f9b9e
OHYeeah <132549049+OHYeeah@users.noreply.github.com>
2023-06-19 15:21:41 +0200

    Add files via upload

--- Dritte Übung while Daniel Humpel.py
+++ Dritte Übung while Daniel Humpel.py
@@ -1,5 +1,15 @@
-a = int(input ("Zahl 1: ")) #int a
-b = int(input ("Zahl 2: ")) #int b
+while True:
+    try:
+        a = int(input ("Zahl 1: ")) #int a
+        break
+    except ValueError:
+        print ("no good")
+while True:
+    try:
+        b = int(input ("Zahl 2: ")) #int b
+        break
+    except ValueError:
+        print("nur Zahlen eingeben bitte")
 
 print ("Sum a+b = ", a+b)
 print ("Dif a-b = ", a-b)
